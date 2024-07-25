6ce737de099d81730e88a50846d9cf82de7e4f66
sora <dongnaitian@gmail.com>
2024-07-25 15:35:26 +0800

    撤销使用webpack插件

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
